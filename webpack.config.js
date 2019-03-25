5ee6abeba964b126a312278c064c0d6b56598a07
leemyunggyu <jaylee@hatiolab.com>
2019-03-25 15:51:00 +0900

    configure babel & webpack

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+const OUTPUT_PATH = 'dist'
+module.exports = {
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.join(__dirname, OUTPUT_PATH),
+    port: 8080
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, 'index.html')
+    })
+  ]
+};
