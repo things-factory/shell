9693cff19b6dcf7863ecf215600e2a779a38315e
heartyoh <heartyoh@hatiolab.com>
2019-05-03 11:47:27 +0800

    add things-scene-components-with-tools.import

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -94,6 +94,20 @@ module.exports = {
             }
           }
         ]
+      },
+      {
+        test: /things-scene-components-with-tools.import$/,
+        use: [
+          {
+            loader: 'babel-loader'
+          },
+          {
+            loader: 'things-scene-config-webpack-loader',
+            options: {
+              module_path: nodeModulePath
+            }
+          }
+        ]
       }
     ]
   },
