8338bbd612c3644f3b1b3516713f23add00be9aa
Hearty Oh <heartyoh@hatiolab.com>
2019-05-05 15:25:54 +0800

    v0.2.17

--- server/entities/user.ts
+++ server/entities/user.ts
@@ -2,12 +2,12 @@ import { Entity, Column, PrimaryColumn, CreateDateColumn, UpdateDateColumn, getR
 
 import crypto from 'crypto'
 import jwt from 'jsonwebtoken'
-import { DomainBaseEntity } from './domain-base-entity'
+import { BaseEntity } from './base-entity'
 
 const SECRET = '0xD58F835B69D207A76CC5F84a70a1D0d4C79dAC95'
 
 @Entity('user')
-export class User extends DomainBaseEntity {
+export class User extends BaseEntity {
   @Column('text')
   email: string
 
