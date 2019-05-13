356d4306decce72f055fb330bd47fc9c8ea1b92b
Hearty Oh <heartyoh@hatiolab.com>
2019-05-13 16:23:07 +0900

    v0.2.44

--- ormconfig.js
+++ ormconfig.js
@@ -4,7 +4,7 @@ module.exports = {
   database: 'db.sqlite',
   synchronize: true,
   logging: true,
-  entities: ['dist-server/entities/**/*.js'],
-  migrations: ['dist-server/migrations/**/*.js'],
-  subscribers: ['dist-server/subscribers/**/*.js']
+  entities: ['dist-server/entities/**/*.js', 'node_modules/@things-factory/*/dist-server/entities/**/*.js'],
+  migrations: ['dist-server/migrations/**/*.js', 'node_modules/@things-factory/*/dist-server/migrations/**/*.js'],
+  subscribers: ['dist-server/subscribers/**/*.js', 'node_modules/@things-factory/*/dist-server/subscribers/**/*.js']
 }
