56736e6c1403a089fdc19a3a6108a8d6859fda43
leemyunggyu <jaylee@hatiolab.com>
2019-05-29 20:50:29 +0900

    v0.2.81

---
+++ server/graphql/condition-builder.ts
@@ -0,0 +1,103 @@
+export const conditionBuilder = function(fieldName: string, operator: string, value: any) {
+  switch (operator) {
+    case 'eq':
+      return {
+        clause: `${fieldName} = :${fieldName}`,
+        parameters: { [fieldName]: value }
+      }
+
+    case 'like':
+      return {
+        clause: `${fieldName} LIKE :${fieldName}`,
+        parameters: { [fieldName]: `%${value}%` }
+      }
+
+    case 'nlike':
+      return {
+        clause: `${fieldName} NOT LIKE :${fieldName}`,
+        value: { [fieldName]: `(${value})` }
+      }
+
+    case 'lt':
+      return {
+        clause: `${fieldName} < :${fieldName}`,
+        parameters: { [fieldName]: value }
+      }
+
+    case 'gt':
+      return {
+        clause: `${fieldName} > :${fieldName}`,
+        parameters: { [fieldName]: value }
+      }
+
+    case 'lte':
+      return {
+        clause: `${fieldName} <= :${fieldName}`,
+        parameters: { [fieldName]: value }
+      }
+
+    case 'gte':
+      return {
+        clause: `${fieldName} >= :${fieldName}`,
+        parameters: { [fieldName]: value }
+      }
+
+    case 'noteq':
+      return {
+        clause: `${fieldName} != :${fieldName}`,
+        parameters: { [fieldName]: value }
+      }
+
+    case 'in':
+      return {
+        clause: `${fieldName} IN :${fieldName}`,
+        parameters: { [fieldName]: `(${value})` }
+      }
+
+    case 'notin':
+      return {
+        clause: `${fieldName} NOT IN :${fieldName}`,
+        parameters: { [fieldName]: `(${value})` }
+      }
+
+    case 'is_null':
+      return {
+        clause: `${fieldName} IS NULL`
+      }
+    case 'is_not_null':
+      return {
+        clause: `${fieldName} IS NOT NULL`
+      }
+    case 'is_false':
+      return {
+        clause: `${fieldName} IS FALSE`
+      }
+    case 'is_true':
+      return {
+        clause: `${fieldName} IS TRUE`
+      }
+    case 'is_not_false':
+      return {
+        clause: `${fieldName} IS NOT FALSE`
+      }
+    case 'is_not_true':
+      return {
+        clause: `${fieldName} IS NOT TRUE`
+      }
+    case 'is_present':
+      return {
+        clause: `${fieldName} IS PRESENT`
+      }
+    case 'is_blank':
+      return {
+        clause: `${fieldName} IS BLANK`
+      }
+    case 'is_empty_num_id':
+      return {
+        clause: `${fieldName} IS EMPTY NUMERIC ID`
+      }
+
+    case 'between':
+      return {}
+  }
+}
--- server/graphql/index.ts
+++ server/graphql/index.ts
@@ -1,5 +1,6 @@
 import * as typeDefs from './types'
 import * as resolvers from './resolvers'
+export { conditionBuilder } from './condition-builder'
 
 export const schema = {
   typeDefs,
