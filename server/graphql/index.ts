8c006e0200ed9662dfda831e848ee7f684059cc0
leemyunggyu <jaylee@hatiolab.com>
2019-05-30 10:48:24 +0900

    v0.2.85

--- server/graphql/index.ts
+++ server/graphql/index.ts
@@ -1,6 +1,7 @@
 import * as typeDefs from './types'
 import * as resolvers from './resolvers'
 export { conditionBuilder } from './condition-builder'
+export { Filter, Pagination } from './types'
 
 export const schema = {
   typeDefs,
