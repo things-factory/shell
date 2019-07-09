93ecca6741289d24b715a3e29156a37b2f78f705
Hearty Oh <heartyoh@hatiolab.com>
2019-07-09 22:15:27 +0900

    remove DomainBaseEntity

--- server/entities/domain-base-entity.ts
+++
@@ -1,9 +0,0 @@
-import { Column, ManyToOne, PrimaryGeneratedColumn, CreateDateColumn } from 'typeorm'
-import { Domain } from './domain'
-
-import { BaseEntity } from './base-entity'
-
-export abstract class DomainBaseEntity extends BaseEntity {
-  @ManyToOne(type => Domain)
-  domain: Domain
-}
