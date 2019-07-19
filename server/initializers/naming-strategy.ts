92912eb125edfdd970a912d0c5ca2279d30c4e15
leemyunggyu <jaylee@hatiolab.com>
2019-07-19 15:32:46 +0900

    v0.5.60
    fix naming strategy

--- server/initializers/naming-strategy.ts
+++ server/initializers/naming-strategy.ts
@@ -1,10 +1,11 @@
 import { NamingStrategyInterface } from 'typeorm'
 import { DefaultNamingStrategy } from 'typeorm'
+import pluralize from 'pluralize'
 import _ from 'lodash'
 
 export class NamingStrategy extends DefaultNamingStrategy implements NamingStrategyInterface {
   tableName(targetName: string, userSpecifiedName: string): string {
-    return userSpecifiedName || _.snakeCase(targetName)
+    return userSpecifiedName || pluralize(_.snakeCase(targetName))
   }
 
   columnName(propertyName: string, customName: string, embeddedPrefixes: string[]): string {
