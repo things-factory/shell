3de3b1dff8de92c71f180ab3a7f7d2de3f586509
leemyunggyu <jaylee@hatiolab.com>
2019-09-03 11:46:36 +0900

    v0.5.145

--- server/server-context.ts
+++ server/server-context.ts
@@ -1,8 +1,10 @@
 export async function context({ ctx }) {
+  // TODO: 서브도메인으로 도메인 판별 가능할 경우 해당 정보를 바탕으로
+  //       ctx의 도메인을 초기화 하도록 수정해야 함
   const domain = ctx && ctx.state && ctx.state.user && ctx.state.user.domain
 
   if (domain) {
-    ctx.domain = domain
+    ctx.state.domain = domain
   }
 
   return ctx
