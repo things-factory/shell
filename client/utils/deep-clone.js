da0a97afc6ab46270f61581e83a1ac15b3e3f475
Murky <moosung.gil@hatiolab.com>
2019-10-22 19:36:06 +0900

    use lodash's in deep-clone.js

--- client/utils/deep-clone.js
+++ client/utils/deep-clone.js
@@ -1,7 +1,3 @@
-export function deepClone(o) {
-  return JSON.parse(JSON.stringify(o))
-}
-
-export function deepEquals(o1, o2) {
-  return JSON.stringify(o1) == JSON.stringify(o2)
-}
+import deepClone from 'lodash/cloneDeep'
+import deepEquals from 'lodash/isEqual'
+export { deepClone, deepEquals }
