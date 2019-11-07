d0de8715d21b99c31c45e41654a851f7f5c54f23
Murky <moosung.gil@hatiolab.com>
2019-11-07 12:33:26 +0900

    server-context의 domain 정보를 url에서 부터 가져오도록 변경

--- server/server-context.ts
+++ server/server-context.ts
@@ -1,14 +1,31 @@
 import { getPathInfo } from './utils/context-path'
+import { getRepository } from 'typeorm'
+import { Domain } from './entities/domain'
 export async function context({ ctx }) {
   // TODO: 서브도메인으로 도메인 판별 가능할 경우 해당 정보를 바탕으로
   //       ctx의 도메인을 초기화 하도록 수정해야 함
 
   var { request } = ctx
-  var { origin, referer } = request
+  var { origin, header } = request
+  var { referer } = header
   var pathname = referer.replace(origin, '')
   var { domain } = getPathInfo(pathname)
 
-  if (domain) ctx.state.domain = domain
+  var domainObj = {}
+
+  if (domain) {
+    var repo = getRepository(Domain)
+    var d = await repo.findOne({
+      where: [{ subdomain: domain }],
+      cache: true
+    })
+
+    if (d) {
+      domainObj = d
+    }
+  }
+
+  ctx.state.domain = domainObj
 
   return ctx
 }
