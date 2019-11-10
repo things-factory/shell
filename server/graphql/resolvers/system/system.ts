b98f9586d2bff5a45cfc994f538ed3585d50b1dc
Hearty Oh <heartyoh@hatiolab.com>
2019-11-11 00:29:44 +0900

    Update system.ts

--- server/graphql/resolvers/system/system.ts
+++ server/graphql/resolvers/system/system.ts
@@ -3,9 +3,9 @@ import { pubsub } from '../../../pubsub'
 export const systemRebooted = {
   systemRebooted: {
     /* subscription payload can be filtered here */
-    resolve(payload, args) {
-      return payload.systemRebooted
-    },
+    // resolve(payload, args) {
+    //   return payload.systemRebooted
+    // },
     subscribe(_, args, { ctx }) {
       /* it is possible to check authentication here */
       // if (!ctx.user) {
