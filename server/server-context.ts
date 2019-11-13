a94dc25974c6e03f82ebf6545b7cd5b75d0b6f21
Murky <moosung.gil@hatiolab.com>
2019-11-13 12:34:30 +0900

    get pathname using URL api

--- server/server-context.ts
+++ server/server-context.ts
@@ -1,14 +1,15 @@
-import { getPathInfo } from './utils/context-path'
 import { getRepository } from 'typeorm'
+import { URL } from 'url'
 import { Domain } from './entities/domain'
+import { getPathInfo } from './utils/context-path'
 export async function context({ ctx }) {
   // TODO: 서브도메인으로 도메인 판별 가능할 경우 해당 정보를 바탕으로
   //       ctx의 도메인을 초기화 하도록 수정해야 함
 
   var { request } = ctx
   var { header } = request
-  var { origin, referer } = header
-  var pathname = referer.replace(origin, '')
+  var { referer } = header
+  var { pathname } = new URL(referer)
   var { domain } = getPathInfo(pathname)
 
   var domainObj = {}
