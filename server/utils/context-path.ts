71d724b4a194897b41c48e6e730b6cec50dad575
Murky <moosung.gil@hatiolab.com>
2020-01-16 19:31:41 +0900

    use things-factory/utils

--- server/utils/context-path.ts
+++ server/utils/context-path.ts
@@ -1,19 +1,9 @@
-import { CONTEXT_PATH_PREFIX } from '../context-path-prefix'
+import * as Utils from '@things-factory/utils'
 
 export function getPathInfo(pathname) {
-  var regexp = new RegExp(`(/${CONTEXT_PATH_PREFIX}/(\\w+))?(/?.*)`)
-  var matched = pathname.match(regexp)
-  var contextPath = matched[1] || ''
-  var domain = matched[2] || ''
-  var path = matched[3]
-
-  return {
-    contextPath,
-    domain,
-    path
-  }
+  return Utils.getPathInfo(pathname)
 }
 
 export function makeContextPath(context) {
-  return `${CONTEXT_PATH_PREFIX}/${context}`
+  return Utils.makeContextPath(context)
 }
