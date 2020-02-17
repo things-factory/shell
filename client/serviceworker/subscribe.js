fa5935d88b5833bfba45610d13d50908cda2c06f
Murky <moosung.gil@hatiolab.com>
2020-02-17 16:19:17 +0900

    web notification 중복 등록 방지
    #33

--- client/serviceworker/subscribe.js
+++ client/serviceworker/subscribe.js
@@ -1,23 +1,20 @@
 import { urlBase64ToUint8Array } from './url-base64-to-uint8-array'
-import { unsubscribe } from './unsubscribe'
 
 export async function subscribe(userInfo) {
   try {
     var registration = await navigator.serviceWorker.ready
     var subscription = await registration.pushManager.getSubscription()
 
-    if (subscription) {
-      unsubscribe()
+    if (!subscription) {
+      const response = await fetch('/vapidPublicKey')
+      const vapidPublicKey = await response.text()
+      const convertedVapidKey = urlBase64ToUint8Array(vapidPublicKey)
+
+      subscription = await registration.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: convertedVapidKey
+      })
     }
-
-    const response = await fetch('/vapidPublicKey')
-    const vapidPublicKey = await response.text()
-    const convertedVapidKey = urlBase64ToUint8Array(vapidPublicKey)
-
-    subscription = await registration.pushManager.subscribe({
-      userVisibleOnly: true,
-      applicationServerKey: convertedVapidKey
-    })
 
     if (!subscription) return false
     if (!userInfo) return
