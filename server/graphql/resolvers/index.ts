e979416db275291284f250a36d618f108fdda39c
Hearty Oh <heartyoh@hatiolab.com>
2020-02-27 21:18:57 +0900

    remove SystemRebooted Subscription

--- server/graphql/resolvers/index.ts
+++ server/graphql/resolvers/index.ts
@@ -1,8 +1,7 @@
 import * as Domain from './domain'
-import * as System from './system'
 
 export const queries = [Domain.Query]
 
 export const mutations = [Domain.Mutation]
 
-export const subscriptions = [System.Subscription]
+export const subscriptions = []
