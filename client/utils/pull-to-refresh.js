6fdc75aab97b1bbad434fa37fa36bbacf6ac55c3
Hearty, Oh <heartyoh@hatiolab.com>
2020-06-07 21:59:22 +0900

    move shell/utils to utils & styles

--- client/utils/pull-to-refresh.js
+++ client/utils/pull-to-refresh.js
@@ -1,5 +1,7 @@
 import * as ptr from '@things-factory/utils'
 
+/* TOBE-REMOVED 2020년 6월 7일 현재, 사용되지 않고 있음을 확인 */
+
 export function pulltorefresh(opts) {
   console.warn(`pulltorefresh is deprecated. please use @things-factory/utils's instead of @things-factory/shell's`)
   return ptr.pulltorefresh(opts)
