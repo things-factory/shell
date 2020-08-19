01c04e85299beb26d40fcc524d4b18f5e027cd4b
Hearty Oh <heartyoh@hatiolab.com>
2020-08-19 10:00:17 +0900

    fix migration

--- server/migrations/1000000000000-SeedDomain.ts
+++ server/migrations/1000000000000-SeedDomain.ts
@@ -13,23 +13,23 @@ export class SeedDomain1000000000000 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<any> {
     const repository = getRepository(Domain)
 
-    try {
-      SEED_DOMAINS.forEach(async domain => {
+    return Promise.all(
+      SEED_DOMAINS.map(async domain => {
         await repository.save({
           ...domain
         })
       })
-    } catch (e) {
-      console.error(e)
-    }
+    )
   }
 
   public async down(queryRunner: QueryRunner): Promise<any> {
     const repository = getRepository(Domain)
 
-    SEED_DOMAINS.reverse().forEach(async domain => {
-      let recode = await repository.findOne({ name: domain.name })
-      await repository.remove(recode)
-    })
+    return Promise.all(
+      SEED_DOMAINS.reverse().map(async domain => {
+        let recode = await repository.findOne({ name: domain.name })
+        await repository.remove(recode)
+      })
+    )
   }
 }
